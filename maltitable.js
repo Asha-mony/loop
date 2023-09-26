677ddd473deddcabecc70948b14d048ec357cdd5
Asha-mony <ynomasha5@gmail.com>
2023-09-27 04:43:40 +0600

    class12

---
+++ maltitable.js
@@ -0,0 +1,69 @@
+// maltiplication table--type of
+// for (let i = 1; i <= 10; i++){
+//     console.log(i * 10);
+// }
+
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(10, 'x', i, '=', (i * 10));
+// }
+
+
+
+// function-declear
+// function mTable (num){
+//     for(let i = 1; i <= 10; i++) {
+//         console.log(num, 'x', i, '=', (i * num));
+
+//     }
+
+//     }
+//     mTable(1)
+//     mTable(2)
+//     mTable(3)
+//     mTable(4)
+//     mTable(5)
+//     mTable(6)
+//     mTable(7)
+//     mTable(8)
+//     mTable(9)
+//     mTable(10)
+
+
+
+// Nested for loop--
+// for(let i = 1; i <= 100; i++){
+//     for( let j =1; j <= 10; j++){
+//         console.log(i, 'x', j, '=', (i * j));
+//     }
+    
+// }
+
+
+// break--
+// for(let i = 1; i <= 20; i++){
+//     console.log(i);
+
+//     if(i == 10){
+//     break;
+//    }
+// }
+
+
+// break--use
+// for(let i = 1; i <= 20; i++){
+   
+//     if(i == 10){
+//     break;
+//    }
+//    console.log(i);
+// }
+
+
+// contitue--
+for(let i = 1; i <= 20; i++){
+    if(i < 10){
+        continue;
+    }
+    console.log(i);
+}
---
+++ object.js
@@ -0,0 +1,9 @@
+let myself = { name: 'Asha', age: 21 , class: 7 , roll: 184083 , Address: 'Rajshahi' }
+let mobileDevice = { name: 'Samsung', Model: 'm11', manufacture: 'Taiwan', price: 100000 }
+
+// console.log(myself);
+// console.log(mobileDevice);
+
+console.log(myself.roll);
+console.log(myself['name']);
+console.log(mobileDevice.name);
